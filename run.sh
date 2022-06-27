44bbccb16cef5e8dde0e826ae11b9b6b22b71f6f
namphuongtran9196 <namphuongtran9196@gmail.com>
2022-06-27 21:14:21 +0700

    fix bug

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash --login
 /etc/init.d/nginx start
 uwsgi --ini /usr/vassals/uwsgi.ini
-uwsgi --emperor /usr/vassals/ --uid www-data --gid www-data -d
+uwsgi --emperor /usr/vassals/ --uid www-data --gid www-data
